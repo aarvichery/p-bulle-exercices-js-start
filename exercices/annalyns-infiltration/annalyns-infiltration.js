33f32b81fb379d1c05fbe4f207b9cada6d5b3872
Aaron Vichery <pa84igb@eduvaud.ch>
2025-11-07 15:48:04 +0100

    feat(annalyns-infiltration):Terminer l'exercice
    [20][Done]

--- exercices/annalyns-infiltration/annalyns-infiltration.js
+++ exercices/annalyns-infiltration/annalyns-infiltration.js
@@ -27,7 +27,14 @@
  * @return {boolean} Whether or not you can execute a fast attack.
  */
 export function canExecuteFastAttack(knightIsAwake) {
-  throw new Error('Remove this line and implement the function');
+  if(knightIsAwake == false)
+    {
+      return true
+    }
+    else
+    {
+      return false
+    }
 }
 
 /**
@@ -40,7 +47,14 @@ export function canExecuteFastAttack(knightIsAwake) {
  * @returns {boolean} Whether or not you can spy on someone.
  */
 export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
-  throw new Error('Remove this line and implement the function');
+  if(knightIsAwake == true || archerIsAwake == true || prisonerIsAwake == true)
+  {
+    return true
+  }
+  else
+  {
+    return false
+  }
 }
 
 /**
@@ -52,7 +66,14 @@ export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
  * @returns {boolean} Whether or not you can send a signal to the prisoner.
  */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  throw new Error('Remove this line and implement the function');
+  if(archerIsAwake == false && prisonerIsAwake == true)
+  {
+    return true
+  }
+  else
+  {
+    return false
+  }
 }
 
 /**
@@ -71,5 +92,12 @@ export function canFreePrisoner(
   prisonerIsAwake,
   petDogIsPresent,
 ) {
-  throw new Error('Remove this line and implement the function');
+  if(petDogIsPresent == true && archerIsAwake == false || petDogIsPresent == false && prisonerIsAwake == true && archerIsAwake == false && knightIsAwake == false)
+  {
+    return true
+  }
+  else
+  {
+    return false
+  }
 }
