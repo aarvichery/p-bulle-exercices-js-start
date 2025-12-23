89f0540dcbb292b80ab9482104c4ceab54c2ba17
aarvichery <pa84igb@eduvaud.ch>
2025-12-23 17:18:56 +0100

    Update(mixed-juices):Terminer l'excercice

--- exercices/mixed-juices/mixed-juices.js
+++ exercices/mixed-juices/mixed-juices.js
@@ -42,8 +42,36 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error("Remove this line and implement the function");
+  let limescut = 0;
+  let isok = false;
+  let idx = 0;
+  for(let i = 0; i <= limes.length; i++)
+  {
+    if (i != limes.length && limescut < wedgesNeeded)
+    {
+      switch(limes[i])
+      {
+        case "small":
+          limescut += 6;
+          break;
+        case "medium":
+          limescut += 8;
+          break;
+        case "large":
+          limescut += 10;
+          break;
+      }
+    }
+    else if (isok == false || i == limes.length && limescut < wedgesNeeded)
+    {
+      isok = true;
+      idx += i;
+    }
+  }
+
+  return idx;
 }
+
 
 /**
  * Determines which juices still need to be prepared after the end of the shift.
@@ -53,5 +81,39 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error("Remove this line and implement the function");
+  let final = 0;
+  let tablecomm = [];
+  for(let i = 0; i < orders.length; i++)
+  {
+    if (final < timeLeft)
+    {
+      switch (orders[i]) {
+    case "Pure Strawberry Joy":
+      final += 0.5;
+      break;
+    case "Energizer":
+      final += 1.5;
+      break;
+    case "Green Garden":
+      final += 1.5;
+      break;
+    case "Tropical Island":
+      final += 3;
+      break;
+    case "All or Nothing":
+      final += 5;
+      break;
+    default:
+      final += 2.5;
+      break;
+      }
+      
+    }
+    else
+      {
+        tablecomm.push(orders[i]);
+      }
+    
+    }
+    return tablecomm;
 }
