6c692ba6249f3cc6a5dc3a131f31654b28bba0ce
Aaron Vichery <pa84igb@eduvaud.ch>
2026-01-09 10:11:27 +0100

    Update(vehicle-purchase): Terminer la remédiation
    [10][Done]

--- exercices/vehicle-purchase/vehicle-purchase.js
+++ exercices/vehicle-purchase/vehicle-purchase.js
@@ -23,11 +23,7 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  if (option1 < option2) {
-    return option1 + " is clearly the better choice.";
-  } else {
-    return option2 + " is clearly the better choice.";
-  }
+  return option1 < option2 ? option1 + " is clearly the better choice." : option2 + " is clearly the better choice.";
 }
 
 /**
